8dbfee425e1747915579e2de0d0884ac55d516e0
Le-manh-duy <93904455+Le-manh-duy@users.noreply.github.com>
2022-07-18 14:55:04 +0700

    baitap

---
+++ management/config.php
@@ -0,0 +1,9 @@
+<?php
+$dataHost = 'localhost';
+$dataName = 'studentdb1';
+$dataUserName = 'root';
+$dataPassWord = '';
+
+$mysqli = mysqli_connect($dataHost,$dataUserName,$dataPassWord,$dataName);
+
+?>
